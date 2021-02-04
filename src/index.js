eeee35ad60997913cb054c80a358d5d6971ad746
Daria Viter <viter.daria@gmail.com>
2021-02-04 14:24:41 +0000

    commit

--- src/index.js
+++ src/index.js
@@ -2,15 +2,9 @@ import './scss/styles.scss';
 import fetchService from './js/fetch-service.js';
 import debounce from 'lodash.debounce';
 
-
-
-
 document.querySelector('input').addEventListener(
-  'input', findCountry
-  
-);
-
-const findCountry = debounce((event)=>{
-   
+  'input',
+  debounce(event => {
     fetchService(event.target.value);
-  }, 500)
+  }, 500),
+);
